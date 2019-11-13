8d9d40e8410456ee3bfb481d1de39bc090e0e577
Javier Rodriguez <48281251+JavierRodriguez@users.noreply.github.com>
2019-11-13 11:14:18 +0100

    Iniciación a ListView
    He empezado hacer los ListView para las pantallas de historial y favoritos, ahora mismo no puedo introducir datos ya que deberian de ingresarse de manera dinamica, llegará en un futuro.

--- app/src/main/java/es/losinutiles/docpocket/TabHistorial.java
+++ app/src/main/java/es/losinutiles/docpocket/TabHistorial.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,19 +14,34 @@ import androidx.fragment.app.Fragment;
 
 public class TabHistorial extends Fragment {
     private Button boton1;
+    /**
+     * Aqui introducimos los valores al listView.
+     */
+    ListView lista;
+    String [][] datos={
+            {"PruebaVariable","5 dias"},
+            {"PruebaVariable","6 dias"},
+            {"PruebaVariable","7 dias"},
+            {"PruebaVariable","9 dias"},
+            {"PruebaVariable","16 dias"},
+
+    };
+    //Todavia no puedo inicializar este array hasta que no tengamos
+    //las imagenes guardadas del escaner.
+
+    int[] datosImagenes;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tab_historial, container, false);
-        boton1 = view.findViewById(R.id.boton1);
 
-        boton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Ha hecho clic en el boton del fragmento 1", Toast.LENGTH_SHORT).show();
-            }
-        });
+        lista=view.findViewById(R.id.idLista);
+
         return view;
     }
+
+
+
 }
