1204b66e531bbf47501fc44c66e5339be83e1c89
makitren <alfremc13@gmail.com>
2019-12-11 01:06:51 +0100

    Documentadas las clases que faltaban

--- app/src/main/java/es/losinutiles/docpocket/VisorImagen.java
+++ app/src/main/java/es/losinutiles/docpocket/VisorImagen.java
@@ -10,6 +10,11 @@ public class VisorImagen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        /*
+         *VisorImagen se encarga de generar el Layout de la imagen seleccionada, como transición
+         * hacia la ventana con los botones para realizar la busqueda
+         */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visor_imagen);
         ImageView img=(ImageView)findViewById(R.id.visorImagenCompleta);
